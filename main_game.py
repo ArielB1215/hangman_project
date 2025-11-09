9beb340be3267c4d193c9d17135c47f59fa03da7
Ariel Ben Michael <abm2511@gmail.com>
2025-11-09 20:15:43 +0200

    created random word function

--- main_game.py
+++ main_game.py
@@ -1,1 +1,34 @@
+import random
+
 print("Welcome to the Hangman game!")
+
+list_of_words = [
+    "banana",
+    "monkey",
+    "zoom",
+    "cat",
+    "dog",
+    "avocado",
+    "epic"
+]
+
+
+def choose_a_random_word(word_list: list):
+    random_index = random.randint(a=0, b=len(word_list) - 1)
+    return list_of_words[random_index]
+
+def print_hidden_word(word):
+    ...
+
+
+if __name__ == '__main__':
+    print(choose_a_random_word(list_of_words))
+
+
+
+
+
+
+
+
+
