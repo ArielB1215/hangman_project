88cb2b3c2ef4b8713babd3e2a92e8695a2b2e22c
Ariel Ben Michael <abm2511@gmail.com>
2025-11-09 21:38:25 +0200

    added enter letter function

--- main_game.py
+++ main_game.py
@@ -18,12 +18,19 @@ def choose_a_random_word(word_list: list):
     random_index = random.randint(a=0, b=len(word_list) - 1)
     return list_of_words[random_index]
 
-def print_hidden_word(word):
-    ...
+def print_hidden_word(word: str):
+    if word.isalpha() == False:
+            return False
+    elif len(word) != 1:
+        return False
+    else:
+        return word
 
 
 if __name__ == '__main__':
     print(choose_a_random_word(list_of_words))
+    new_word = input("Enter a new letter: ")
+    print(print_hidden_word(new_word))
 
 
 
